7bbeae4622094103a2c9c394b304467024c8952c
Luis Malta <luiso.malta@gmail.com>
2020-06-27 09:19:39 -0300

    Initial Commit

---
+++ trabalho-final-web/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { CartComponent } from './pages/cart/cart.component'
+import { ConfigurationComponent } from './pages/configuration/configuration.component'
+import { DessertComponent } from './pages/dessert/dessert.component'
+import { DrinkComponent } from './pages/drink/drink.component'
+import { LoginComponent } from './pages/login/login.component'
+import { MainComponent } from './pages/main/main.component'
+
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'main', component: MainComponent },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ trabalho-final-web/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { MealComponent } from './pages/meal/meal.component';
+import { DrinkComponent } from './pages/drink/drink.component';
+import { DessertComponent } from './pages/dessert/dessert.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { RevenuesComponent } from './pages/revenues/revenues.component';
+import { ConfigurationComponent } from './pages/configuration/configuration.component';
+import { AppRoutingModule } from './app-routing.module';
+import { MainComponent } from './pages/main/main.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    MealComponent,
+    DrinkComponent,
+    DessertComponent,
+    CartComponent,
+    RevenuesComponent,
+    ConfigurationComponent,
+    MainComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
