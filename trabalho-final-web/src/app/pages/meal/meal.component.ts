250aef4660e69c57f3ef2d33ab9635c24844b1cd
Luis Malta <luiso.malta@gmail.com>
2020-06-29 14:47:16 -0300

    [ADD] Meal Page

--- trabalho-final-web/src/app/pages/meal/meal.component.ts
+++ trabalho-final-web/src/app/pages/meal/meal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Meal } from './meal'
 
 @Component({
   selector: 'app-meal',
@@ -9,7 +10,33 @@ export class MealComponent implements OnInit {
 
   constructor() { }
 
+  mainItens = ['Arroz', 'Lentilha']
+  meatItens = ['Bisteca', 'Frango', 'Carne de panela']
+  sideDishItens = ['Pure de batata', 'Salpicão']
+  cart = []
+  mealOption = new Meal('', '', '')
+  mealQunty = 1
+
+
+  addCart(meal, mealQunty){
+        console.log(meal + " - " + mealQunty)
+
+    var item = {
+      item: meal,
+      qunty: mealQunty,
+
+    }
+    console.log(this.cart)
+    this.cart.push(item)
+    sessionStorage.setItem('cart', JSON.stringify(this.cart))
+    console.log(this.cart)
+  }
+
   ngOnInit(): void {
+    if (JSON.parse(sessionStorage.getItem('cart')) != null){
+      this.cart = JSON.parse(sessionStorage.getItem('cart'))
+    }
+    console.log(this.cart)
   }
 
 }
