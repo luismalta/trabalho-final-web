c36220bb6ff7f5c7eec0a78ed676d6644bb99e26
Luis Malta <luiso.malta@gmail.com>
2020-06-29 14:46:47 -0300

    [ADD] Main page

--- trabalho-final-web/src/app/pages/main/main.component.ts
+++ trabalho-final-web/src/app/pages/main/main.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
 })
 export class MainComponent implements OnInit {
 
   constructor() { }
 
+  cart = JSON.parse(sessionStorage.getItem('cart'))
+
   ngOnInit(): void {
+    console.log(this.cart)
   }
 
 }
